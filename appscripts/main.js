fefeafcc3e0609d094a60acff65fb92cd6fcdca5
pang-xue-le-cheryl-nm2207 <125462621+pang-xue-le-cheryl-nm2207@users.noreply.github.com>
2023-03-27 12:08:20 +0800

    Add files via upload

--- appscripts/main.js
+++ appscripts/main.js
@@ -5,32 +5,46 @@ const increasingSinglesLineChart = {
     labels: years,
     datasets: [
         {
-            label: "20-24 Years | Male",
-            data: [1,2,3,4],
+            label: "20-24 Years",
+            data: [93.2, 93, 93.1, 91.5, 93.3, 93.1, 94.4, 95.1, 94.8, 96.1, 96.4, 96.7, 97.7, 97.2, 96.9, 96.7, 97.1, 97.1, 97.1, 96.9, 97.8],
             borderWidth: 2,
             backgroundColor: "transparent",
-            borderColor: "rgb(255, 153, 153)"
+            borderColor: "rgba(239,187,185,255)"
         },
         {
-            label: "20-24 Years | Female",
-            data: [5,4,5,6,9],
+            label: "25-29",
+            data: [57.5, 56.7, 60.2, 58.1, 62.7, 63, 62.6, 65.1, 64, 67.1, 70.5, 71.3, 74.3, 71.4, 71.2, 71.7, 72.8, 73.9, 75.3, 77.1, 75.8],
             borderWidth: 2,
             backgroundColor: "transparent",
-            borderColor: "rgb(255, 204, 153)"
+            borderColor: "rgba(246,198,149,255)"
         },
         {
-            label: "25-34 Years | Male",
-            data: [5,1,3,4],
+            label: "30-34 Years",
+            data: [25.9, 26.5, 26.7, 27.7, 28.3, 27.9, 29.3, 29.5, 30.8, 31.1, 31.6, 32.9, 32.1, 31.2, 31.8, 32.4, 33.5, 34.8, 37.1, 40.3, 36.3],
             borderWidth: 2,
             backgroundColor: "transparent",
-            borderColor: "rgb(171, 217, 126)"
+            borderColor: "rgba(251,229,182,255)"
         },
         {
-            label: "25-34 Years | Female",
-            data: [10,3,2,6,9],
+            label: "35-39 Years",
+            data: [16.5, 18.6, 16.1, 17.3, 17.5, 17, 17.6, 17.3, 18.7, 18.4, 17.6, 19.4, 20.2, 18.7, 18.8, 18.3, 18.3, 20.9, 21.6, 20.4],
             borderWidth: 2,
             backgroundColor: "transparent",
-            borderColor: "rgb(153, 204, 255)"
+            borderColor: "rgba(203,222,171,255)"
+        },
+        {
+            label: "40-44 Years",
+            data: [14.5, 14.9, 13.8, 14.7, 13.8, 13.9, 13.3, 13.9, 14.7, 14.3, 12.6, 15.2, 15.3, 14.8, 14.7, 15.4, 14.4, 17.4, 16.6, 14.7, 16.3],
+            borderWidth: 2,
+            backgroundColor: "transparent",
+            borderColor: "rgba(212,245,254,255)"
+        },
+        {
+            label: "45-49 Years",
+            data: [12, 12.6, 12.5, 13.1, 12.9, 12.4, 12.2, 12.2, 13, 12.3, 12.7, 12.7, 13.1, 13.7, 12.7, 13, 13.4, 14.7, 14.9, 14.4, 14.4],
+            borderWidth: 2,
+            backgroundColor: "transparent",
+            borderColor: "rgba(158,129,216,255)"
         },
 
     ]
@@ -43,6 +57,10 @@ new Chart("lineChart", {//!--Notice here we put the id of the "new chart" we cre
         legend: {
             display: true,
             position: 'bottom'
+        },
+        title: {
+            display: true,
+            text: 'Proportion Of Singles Among Resident Population By Selected Age Group'
         },
         responsive: true
     }
@@ -58,8 +76,8 @@ const maleAndFemale = {
     labels: ["male", "female"],
     datasets: [
         {
-            label: "percentage increase",
-            data: [1,2],
+            label: "proportion of singles",
+            data: [98.8, 96.6],
             borderWidth: 2,
             backgroundColor: [
                 "rgb(190,235,231)",
@@ -82,14 +100,17 @@ const config = new Chart("barChart", {//!--Notice here we put the id of the "new
         responsive: false,
         maintainAspectRatio: false,
         legend: {
+            display: false,
+        },
+        title: {
             display: true,
-            position: 'bottom'
+            text: 'Proportion Of Singles Among Resident Population By Gender'
         },
         scales: {
             yAxes: [{
                 ticks: {
                     min:0,
-                    max:8
+                    max:100
                 }
             }]
         }
@@ -102,15 +123,15 @@ function updateChart(option){
     let state = option.value;
     switch(state){
         case "group1":
-            config.data.datasets[0].data = [1, 2];
+            config.data.datasets[0].data = [98.8, 96.6];
             console.log("we are in" + state);
             break;
         case "group2":
-            config.data.datasets[0].data = [3, 4];
+            config.data.datasets[0].data = [82, 69.5];
             console.log("we are in" + state);
             break;
         case "group3":
-            config.data.datasets[0].data = [4, 5];
+            config.data.datasets[0].data = [41.1, 31.8];
             console.log("we are in" + state);
             break;
     }
@@ -143,6 +164,10 @@ new Chart("pieChart", {//!--Notice here we put the id of the "new chart" we crea
     type: "pie",
     data: notDatedBefore,
     options: { // configure options here
+        title: {
+            display: true,
+            text: 'Percentage of singles that :'
+        },
         legend: {
             display: false,
         },
@@ -269,11 +294,11 @@ const data = {
     'Online Channels',
     'At School',
     'Through Friends',
-    'At Word',
+    'At Work',
     'Social Activities',
   ],
   datasets: [{
-    label: 'My First Dataset',
+    label: '2012',
     data: [7, 30, 32, 21, 4],
     fill: true,
     backgroundColor: 'rgba(255, 99, 132, 0.2)',
@@ -283,7 +308,7 @@ const data = {
     pointHoverBackgroundColor: '#rgb(255, 99, 132)',
     pointHoverBorderColor: 'rgb(255, 99, 132)'
   }, {
-    label: 'My Second Dataset',
+    label: '2016',
     data: [13, 26, 27, 20, 5],
     fill: true,
     backgroundColor: 'rgba(255, 159, 64, 0.2)',
@@ -293,7 +318,7 @@ const data = {
     pointHoverBackgroundColor: 'rgb(255, 159, 64)',
     pointHoverBorderColor: 'rgb(255, 159, 64)'
   },{
-    label: 'My Third Dataset',
+    label: '2021',
     data: [29, 21, 18, 16, 9],
     fill: true,
     backgroundColor: 'rgba(255, 205, 86, 0.2)',
@@ -310,9 +335,12 @@ new Chart("radarChart", {//!--Notice here we put the id of the "new chart" we cr
     data: data,
     responsive: true,
     options: { // configure options here\
-        scale: { ticks: {
-            backgroundColor: "#000000",
-        } },
+        scale: {
+            ticks: {
+              display: true,
+              backdropColor: '#BDB7DC',
+            }
+          },
         legend: {
             display: true,
             position:'bottom'
